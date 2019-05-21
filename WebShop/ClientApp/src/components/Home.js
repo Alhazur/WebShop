f933aa18071f106fa61029a6067379b7b67724dc
deltagare <deltagare@LEXJONKAF32>
2019-05-21 16:19:19 +0200

    Add project files.

---
+++ WebShop/ClientApp/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+      <div>
+        <h1>Coop Sweden</h1>
+        <p>Welcome to your new single-page application</p>
+        
+      </div>
+    );
+  }
+}
